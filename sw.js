eb79dfa6c1ffdf80efa22303b7095087800a8f53
e.regas.duncan <e.regas.duncan@gmail.com>
2026-07-04 00:14:38 +0200

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "qr-verifier-v1";
+
+const FILES = [
+    "./",
+"./index.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response =>
+        response || fetch(event.request)
+        )
+    );
+});
